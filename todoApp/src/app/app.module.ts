770eb6654969bea8c0730c88ed153ea8e5e75a70
Chris Chang Change <changc21@gmatc.matc.edu>
2023-12-04 01:43:52 -0600

    linked up firebase with angular

--- todoApp/src/app/app.module.ts
+++ todoApp/src/app/app.module.ts
@@ -3,12 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { provideFirebaseApp, getApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDa0w0rWstfT7jcFHbSy2dLT5vQqDedPMA",
+  authDomain: "todo-app-777dd.firebaseapp.com",
+  projectId: "todo-app-777dd",
+  storageBucket: "todo-app-777dd.appspot.com",
+  messagingSenderId: "577431937280",
+  appId: "1:577431937280:web:3955142cd52197ba8dd3e5"
+};
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    provideFirebaseApp(() => initializeApp( firebaseConfig)),
+    provideFirestore(() => getFirestore()),
   ],
   providers: [],
   bootstrap: [AppComponent]
